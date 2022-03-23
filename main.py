09f6cc32d3fd219f0a312f129064908436290270
S-M-Abrar-Hossa <102193641+S-M-Abrar-Hossain-Asif@users.noreply.github.com>
2022-03-23 01:02:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to the Team Name Generator!")
+city=input("What is the name of your city? ")
+title=input("Write a single word that you want to be in the name: ")
+print("Congratulatios! You have successfully made it! Your team name is: "+city+" " + title)
